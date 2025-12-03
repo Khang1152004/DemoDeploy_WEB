bf9a8c38c5db484de91561d8a0d34e27eefa2bb9
Khang1152004 <khang0867775510@gmail.com>
2025-12-03 17:12:48 +0700

    fix index

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+?>
+<?php
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/core/Database.php';
 require_once __DIR__ . '/core/Auth.php';
